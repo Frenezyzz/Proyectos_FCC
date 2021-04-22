514a2673025b3959de1480cf94f39fe84f7d92cf
Aron <aroneliasiba@gmail.com>
2021-04-22 18:13:48 -0300

    JavaScript Algorithms and Data Structures

---
+++ avaScript Algorithms and Data Structures/Palindrome Checker/Palindrome_Checker.js
@@ -0,0 +1,12 @@
+function palindrome(str) {
+  let polishedString = str.replace(/\W+|_/g,"").toLowerCase();
+  let reversedString = polishedString.split("").reverse().join("");
+  if (polishedString != reversedString){
+    return false;
+  }
+  return true;
+}
+
+
+
+palindrome("eye");
